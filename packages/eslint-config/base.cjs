c3c5fa4e5d2816506614f4bc6c8d86991eca2aff
Michael Cousins <michael.cousins@viam.com>
2023-03-30 16:07:38 -0400

    Add base ESLint config

--- packages/eslint-config/base.cjs
+++ packages/eslint-config/base.cjs
@@ -1,3 +1,25 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'plugin:unicorn/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:sonarjs/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
