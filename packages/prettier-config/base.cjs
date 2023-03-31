8b7bdc9bcf85ae5e30a5aa57db07311eca08181a
Michael Cousins <michael.cousins@viam.com>
2023-03-31 17:58:56 -0400

    fixup: lint

--- packages/prettier-config/base.cjs
+++ packages/prettier-config/base.cjs
@@ -1,21 +1,24 @@
 'use strict';
 
 module.exports = {
-  printWidth: 80, // default
-  tabWidth: 2, // default
-  useTabs: false, // default
-  semi: true, // default
+  // overrides
   singleQuote: true,
-  quoteProps: 'as-needed', // default
   jsxSingleQuote: true,
-  trailingComma: 'es5', // default
-  bracketSpacing: true, // default
-  bracketSameLine: false, // default
-  arrowParens: 'always', // default
-  proseWrap: 'preserve', // default
-  htmlWhitespaceSensitivity: 'css', // default
-  vueIndentScriptAndStyle: false, // default
-  endOfLine: 'lf', // default
-  embeddedLanguageFormatting: 'auto', // default
-  singleAttributePerLine: false, // default
+
+  // default values
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: false,
 };
