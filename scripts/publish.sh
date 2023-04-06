76dc872abf760c576e365760273bf7a3d7ba29fd
Michael Cousins <michael.cousins@viam.com>
2023-04-06 16:55:18 -0400

    Replace pnpm with npm

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,19 +1,66 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 
-# publish all unpublished versions in the workspace
-pnpm publish --recursive --report-summary
+# get the name of a package as specified in its package.json
+function get_name () {
+  package=${1}
 
-# read published packages
-tags=$(jq -r ".publishedPackages[] | .name + \"@\" + .version" < pnpm-publish-summary.json)
+  echo $(jq -r ".name" < "${package}/package.json")
+}
 
-# add a tag for each released version
-for tag in ${tags}; do
-  git tag -a "${tag}" -m "${tag}"
-done
+# get the version of a package as specified in its package.json
+function get_local_version () {
+  package=${1}
 
-# push tags
-for tag in ${tags}; do
-  git push origin "${tag}"
+  echo $(jq -r ".version" < "${package}/package.json")
+}
+
+# get the latest published version of a package in the npm registry
+function get_published_versions () {
+  package=${1}
+  name=${2}
+
+  published_meta=$(npm --workspace="${package}" info --json 2>/dev/null ||:)
+  publish_meta_error=$(echo "${published_meta}" | jq -r ".error.code")
+
+  if [[ "${publish_meta_error}" == "null" ]]; then
+    published_versions=$(echo "${published_meta}" | jq -r ".[\"${name}\"].versions[]")
+
+  elif [[ "${publish_meta_error}" == "E404" ]]; then
+    published_versions=""
+
+  else
+    echo "Unable to read registry for ${package}: ${publish_meta_error}" >&2
+    echo "${published_meta}" >&2
+    exit 1
+  fi
+
+  echo "${published_versions}"
+}
+
+
+function publish_package () {
+  package=${1}
+
+  name=$(get_name "${package}")
+  local_version=$(get_local_version "${package}")
+  published_versions=$(get_published_versions "${package}" "${name}")
+  tag="${name}@${local_version}"
+
+  if [[ "${published_versions}" == *"${local_version}"* ]]; then
+    echo "${tag} is already published"
+  else
+    echo "Publishing ${tag}"
+    npm --workspace "${package}" publish --dry-run
+    git tag -a "${tag}" -m "${tag}"
+  fi
+}
+
+# get a space-separated list of all workspace
+workspaces=$(jq -r ".workspaces[]" < package.json)
+
+# compare the local and published version of each package, and publish if needed
+for package in ${workspaces}; do
+  publish_package "${package}"
 done
