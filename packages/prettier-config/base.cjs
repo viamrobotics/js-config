710af4feb2967021e60de0ce248e9312af3d796f
Michael Cousins <michael.cousins@viam.com>
2023-08-28 17:49:04 -0400

    Switch prettier config back to CJS for compatibility

--- packages/prettier-config/base.cjs
+++ packages/prettier-config/base.cjs
@@ -1,4 +1,6 @@
-export default {
+'use strict';
+
+module.exports = {
   // overrides
   singleQuote: true,
   jsxSingleQuote: true,
--- packages/prettier-config/svelte.cjs
+++ packages/prettier-config/svelte.cjs
@@ -1,6 +1,8 @@
-import baseConfig from './base.js';
+'use strict';
 
-export default {
+const baseConfig = require('./base.cjs');
+
+module.exports = {
   ...baseConfig,
   plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
   svelteIndentScriptAndStyle: false,
