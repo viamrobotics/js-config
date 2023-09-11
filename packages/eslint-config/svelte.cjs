e9a9c658cf79b84c5d975e04f8dc3e14e19469bd
Michael Cousins <michael.cousins@viam.com>
2023-09-11 17:11:37 -0400

    fixup: tweak todo copy

--- packages/eslint-config/svelte.cjs
+++ packages/eslint-config/svelte.cjs
@@ -25,7 +25,6 @@ module.exports = {
       rules: {
         /*
          * TODO(mc, 2023-08-28): this rule is crashing with svelte actions.
-         * Investigate and fix once lint dependencies are updated.
          * https://github.com/sveltejs/eslint-plugin-svelte/issues/583
          */
         'sonarjs/no-unused-collection': 'off',
