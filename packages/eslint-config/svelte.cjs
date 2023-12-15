618c7955f0a8261bb3cc783f2b26fcc2c9bffa3d
Michael Cousins <michael.cousins@viam.com>
2023-12-15 13:18:09 -0500

    Update common configs for Dec. 2023 (#29)
    - Set no-non-null-assertion to error outside of tests
    - Allow `void` statements
    - Allow `= undefined` in .svelte files
    - Loosen rules for .d.ts files

--- packages/eslint-config/svelte.cjs
+++ packages/eslint-config/svelte.cjs
@@ -34,6 +34,9 @@ module.exports = {
          * https://github.com/sveltejs/eslint-plugin-svelte/issues/583
          */
         'sonarjs/no-unused-collection': 'off',
+
+        // Allows us to set option props to `undefined` by default
+        'no-undef-init': 'off',
       },
     },
   ],
