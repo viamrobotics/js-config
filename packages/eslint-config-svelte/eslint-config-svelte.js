4e9d253b59e2a7823fc2fee016ed12f36e239d9b
Michael Cousins <michael.cousins@viam.com>
2025-02-05 14:05:39 -0500

    wip: eslint v9

--- packages/eslint-config-svelte/eslint-config-svelte.js
+++ packages/eslint-config-svelte/eslint-config-svelte.js
@@ -1,66 +1,87 @@
-'use strict';
+import * as svelteParser from 'svelte-eslint-parser';
+import jestDOM from 'eslint-plugin-jest-dom';
+import svelte from 'eslint-plugin-svelte';
+import tailwind from 'eslint-plugin-tailwindcss';
+import testingLibrary from 'eslint-plugin-testing-library';
+import ts from 'typescript-eslint';
 
-const baseConfig = require('@viamrobotics/eslint-config');
+import baseConfig from '@viamrobotics/eslint-config';
 
-module.exports = {
-  ...baseConfig,
-  extends: [
-    ...baseConfig.extends,
-    'plugin:tailwindcss/recommended',
-    'plugin:svelte/recommended',
-    'plugin:svelte/prettier',
-  ],
-  settings: {
-    svelte: {
-      ignoreWarnings: [
-        '@typescript-eslint/no-unsafe-assignment',
-        '@typescript-eslint/no-unsafe-member-access',
+const extraFileExtensions = ['.svelte'];
+
+/**
+ * @typedef {import('@viamrobotics/eslint-config').ConfigArray} ConfigArray
+ */
+
+/** @type {ConfigArray} */
+const config = ts.config(
+  baseConfig,
+  tailwind.configs['flat/recommended'],
+  svelte.configs['flat/recommended'],
+  svelte.configs['flat/prettier'],
+
+  // Base options and settings
+  {
+    languageOptions: {
+      parserOptions: { extraFileExtensions },
+    },
+    settings: {
+      svelte: {
+        ignoreWarnings: [
+          '@typescript-eslint/no-unsafe-assignment',
+          '@typescript-eslint/no-unsafe-member-access',
+        ],
+      },
+      tailwindcss: {
+        callees: ['classnames', 'cx'],
+        classRegex: '^(?:class|cx)$',
+      },
+    },
+  },
+
+  // Svelte
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: { parser: ts.parser, extraFileExtensions },
+    },
+    rules: {
+      // Redundant with `svelte-check` and build
+      'svelte/valid-compile': 'off',
+      // Allows us to set option props to `undefined` by default
+      'no-undef-init': 'off',
+    },
+  },
+
+  // Testing Library DOM assertions
+  {
+    ...jestDOM.configs['flat/recommended'],
+    files: ['**/__tests__/**', '**/*.test.ts', '**/*.spec.ts'],
+  },
+
+  // Testing Library usage
+  {
+    ...testingLibrary.configs['flat/dom'],
+    files: ['**/__tests__/**', '**/*.test.ts', '**/*.spec.ts'],
+    rules: {
+      ...testingLibrary.configs['flat/dom'].rules,
+      'testing-library/await-async-events': [
+        'error',
+        { eventModule: ['fireEvent', 'userEvent'] },
       ],
+      'testing-library/no-await-sync-events': 'off',
+      'testing-library/no-node-access': [
+        'error',
+        { allowContainerFirstChild: true },
+      ],
+      'testing-library/prefer-explicit-assert': [
+        'error',
+        { assertion: 'toBeInTheDocument' },
+      ],
+      'testing-library/prefer-user-event': 'error',
     },
-    tailwindcss: {
-      callees: ['classnames', 'cx'],
-      classRegex: '^(?:class|cx)$',
-    },
-  },
-  parserOptions: {
-    ...baseConfig.parserOptions,
-    extraFileExtensions: ['.svelte'],
-  },
-  overrides: [
-    ...baseConfig.overrides,
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-      },
-      rules: {
-        // Redundant with `svelte-check` and build
-        'svelte/valid-compile': 'off',
-        // Allows us to set option props to `undefined` by default
-        'no-undef-init': 'off',
-      },
-    },
-    // Rules for tests
-    {
-      files: ['**/__tests__/**', '**/*.test.ts', '**/*.spec.ts'],
-      extends: ['plugin:jest-dom/recommended', 'plugin:testing-library/dom'],
-      rules: {
-        'testing-library/await-async-events': [
-          'error',
-          { eventModule: ['fireEvent', 'userEvent'] },
-        ],
-        'testing-library/no-await-sync-events': 'off',
-        'testing-library/no-node-access': [
-          'error',
-          { allowContainerFirstChild: true },
-        ],
-        'testing-library/prefer-explicit-assert': [
-          'error',
-          { assertion: 'toBeInTheDocument' },
-        ],
-        'testing-library/prefer-user-event': 'error',
-      },
-    },
-  ],
-};
+  }
+);
+
+export default config;
